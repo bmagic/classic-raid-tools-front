46aab3361ef17b88d462de575456d782bec530ea
Bmagic <baptiste@bmagic.fr>
2020-05-10 10:09:55 +0200

    Fix some minors bugs

--- src/js/pages/Login/index.js
+++ src/js/pages/Login/index.js
@@ -8,7 +8,7 @@ const Login = () =>
         <Error/>
         <div className='box has-text-centered'>
           <h1 className='title'>Classic Raid Tools</h1>
-          <a className='button' href={`https://discordapp.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${process.env.DISCORD_REDIRECT_URI}&response_type=code&scope=email%20identify`}><span className="icon is-small"><i className='fab fa-discord'/></span><span>Discord</span></a>
+          <a className='button' href={`https://discordapp.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${process.env.DISCORD_REDIRECT_URI}&response_type=code&scope=email%20identify`}><span className="icon is-small"><i className='fab fa-discord'/></span><span>Connexion avec Discord</span></a>
         </div>
       </div>
     </div>
