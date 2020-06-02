a8384d41c14fece3d2d5b2b9f1c10f30a1302564
Bmagic <baptiste@bmagic.fr>
2020-06-02 14:21:53 +0200

    Move Raids menu item to the left.

--- src/js/components/Common/Header/index.js
+++ src/js/components/Common/Header/index.js
@@ -35,8 +35,8 @@ class Header extends React.Component {
             </div>
             <div id="navbar" className={`navbar-menu ${this.state.isOpen ? 'is-active' : ''}`}>
               <div className="navbar-start">
-                <Link to="/roster" className='navbar-item'>Roster</Link>
                 {user && user.roles.includes('member') && <Link to="/raids" className="navbar-item">Raids</Link>}
+                <Link to="/roster" className='navbar-item'>Roster</Link>
                 {user && user.roles.includes('member') && <Link to="/bank" className="navbar-item">Banque de guilde</Link>}
                 {user && user.roles && (user.roles.includes('member') || user.roles.includes('guest')) && <Link to="/presences" className="navbar-item">Présences</Link>}
 
