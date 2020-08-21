43896410100c96b6171507802da70e7cbb5128ce
bmagic <baptiste@bmagic.fr>
2020-08-21 14:38:49 +0200

    Remove unused dependency

--- src/js/pages/Home/index.js
+++ src/js/pages/Home/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Layout from '../../components/Common/Layout'
 import WowRaidImage from '../../components/Common/WowRaidImage'
-import WowClassImage from '../../components/Common/WowClassImage'
 
 const Home = () => {
   return (
